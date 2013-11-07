73536e12fbeddf54aa94e89b23c7ae7c438f6618
Matt Ward <matt.ward@blackboard.com>
2013-11-07 15:39:53 -0800

    participant meter working

--- routes/device.js
+++ routes/device.js
@@ -6,19 +6,6 @@ exports.shake = function(req, res){
   res.render('device/shake', { event: '1' });
 };
 exports.shook = function(req, res){
-	io.sockets.in(req.body.event).emit('message', {motion: req.body.motion, amplitude: req.body.amplitude, frequency: req.body.frequency, count: 10});
+	io.sockets.in(req.body.event).emit('message', {user: req.sessionID, motion: req.body.motion, power: parseFloat(req.body.frequency)*parseFloat(req.body.amplitude)});
 	res.send('OK');
-
-//	pg.connect(connection_string, function(err, client, done) {
-//	  if(err) {
-//	    return console.error('error fetching client from pool', err);
-//	  }
-//	  client.query({text: 'INSERT INTO movements (event_id, motion, amplitude, frequency, created_at) VALUES ($1, $2, $3, $4, CURRENT_TIMESTAMP);', values:[req.body.event, req.body.motion, req.body.amplitude, req.body.frequency]}, function(err, result) {
-//	    done();
-//
-//	    if(err) {
-//	      return console.error('error running query', err);
-//	    }
-//	  });
-//	});
 };
