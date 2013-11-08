da34aa24de8d04b4c0b738591b7cb10c810bbd85
Matt Ward <matt.ward@blackboard.com>
2013-11-08 11:46:32 -0800

    remove stylus

--- app.js
+++ app.js
@@ -35,7 +35,6 @@ app.use(express.cookieParser('your secret here'));
 app.use(express.session());
 app.use(express.bodyParser());
 app.use(app.router);
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 var server = http.createServer(app).listen(app.get('port'));
